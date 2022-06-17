d25667f0e01619d7e73b98503d71691a8a51b22b
GCastilho <castilho.gabriel14@gmail.com>
2022-06-17 19:59:28 -0300

    Implementado handler de teste
    -- Adicionado estrutura da pasta de actions
    -- Adicionado básico do handler de requests
    -- Adicionado um request de createWallet

---
+++ src/actions/createWallet.ts
@@ -0,0 +1,5 @@
+export function createWallet() {
+	return {
+		wallet: 'created'
+	}
+}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import * as actions from './actions'
 
 const {
 	PORT = '3000',
@@ -6,8 +7,21 @@ const {
 
 const app = express()
 
-app.get('/', (_, res) => {
-	res.send({ hello: 'world' })
+app.use(express.json())
+
+app.post('/', async (req, res) => {
+	const { action, ...body } = req.body
+	let handler: undefined|((body: unknown) => unknown|Promise<unknown>) = undefined
+
+	switch (action) {
+		case 'wallet_create': handler = actions.createWallet; break
+	}
+
+	if (handler) {
+		res.status(200).send(await handler(body))
+	} else {
+		res.status(404).send('Method not found')
+	}
 })
 
 app.listen(PORT, () => {
