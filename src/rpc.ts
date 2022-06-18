842352f8d21fba1565accb0a2ebd2d2b96cab24c
GCastilho <castilho.gabriel14@gmail.com>
2022-06-18 17:33:45 -0300

    Adicionado método send para enviar comandos RPC para o node
    -- Renomeado sendo do websocket para wsSend

--- src/rpc.ts
+++ src/rpc.ts
@@ -1,6 +1,10 @@
 import WS from 'ws'
+import axios, { AxiosError } from 'axios'
+import { StatusCodes } from 'http-status-codes'
 import ReconnectingWebSocket from 'reconnecting-websocket'
+import { HttpError } from './errors'
 
+const nanoRpcUrl = process.env.NANO_RPC_URL || 'http://127.0.0.1:55000'
 const nanoSocketUrl = process.env.NANO_SOCKET_URL || 'ws://127.0.0.1:57000'
 
 const ws = new ReconnectingWebSocket(nanoSocketUrl, [], {
@@ -20,7 +24,9 @@ ws.addEventListener('close', () => {
 })
 
 /** Open websocket connection */
-export const open = ws.reconnect
+export function open() {
+	if (ws.readyState == ws.CLOSED) ws.reconnect()
+}
 
 /** Close websocket connection */
 export const close = ws.close
@@ -29,6 +35,22 @@ export const close = ws.close
 export const addEventListener = ws.addEventListener
 
 /** Send message do webscoekt */
-export function send(data: Record<string, unknown>) {
+export function wsSend(data: Record<string, unknown>) {
 	ws.send(JSON.stringify(data))
+}
+
+type JSON = Record<string, unknown>
+export async function send<T = JSON>(data: JSON): Promise<T> {
+	try {
+		const res = await axios.post(nanoRpcUrl, data)
+		if (res.data.error) throw res.data.error
+		return res.data
+	} catch (err) {
+		if (err instanceof AxiosError) {
+			const reason = StatusCodes[err.response?.status as number] || 'INTERNAL_SERVER_ERROR'
+			throw new HttpError(reason as keyof typeof StatusCodes, err.message, err.name)
+		} else {
+			throw err
+		}
+	}
 }
